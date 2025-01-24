77823a14218181c60f94c23d8025a4e3220cbb8a
songhae <boricocke@naver.com>
2025-01-25 01:07:36 +0900

    JPARepository 상속

--- src/main/java/jpa/practice/helloJpa/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package jpa.practice.helloJpa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-    List<Member> findByUserName(String username);
-}
