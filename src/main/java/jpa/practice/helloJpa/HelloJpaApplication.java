875dcd76cd48c11b34ef4ee7c2c5693fb5d1eddf
songhae <boricocke@naver.com>
2025-01-25 01:07:58 +0900

    JPA Auditing

--- src/main/java/jpa/practice/helloJpa/HelloJpaApplication.java
+++ src/main/java/jpa/practice/helloJpa/HelloJpaApplication.java
@@ -2,7 +2,9 @@ package jpa.practice.helloJpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA auditing 활성화  ! 만약 MVC Test를 하게 된다면 에러가 날 수 있음 (JpaConfig 따로 만들어서 application과 분리 필요)
 @SpringBootApplication
 public class HelloJpaApplication {
 
