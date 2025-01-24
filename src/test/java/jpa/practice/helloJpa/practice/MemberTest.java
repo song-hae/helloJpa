9ab7a5d235fd4ccc17260103156d91f99f13c638
songhae <boricocke@naver.com>
2025-01-25 01:06:36 +0900

    test

--- src/test/java/jpa/practice/helloJpa/practice/MemberTest.java
+++ src/test/java/jpa/practice/helloJpa/practice/MemberTest.java
@@ -1,8 +1,8 @@
 package jpa.practice.helloJpa.practice;
 
-import jpa.practice.helloJpa.Member;
-import jpa.practice.helloJpa.MemberRepository;
-import jpa.practice.helloJpa.MemberServiceImpl;
+import jpa.practice.helloJpa.domain.member.Member;
+import jpa.practice.helloJpa.service.MemberServiceImpl;
+import jpa.practice.helloJpa.domain.member.Role;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +14,22 @@ public class MemberTest {
     private MemberServiceImpl memberService;
 
     @Test
-    void test(){
-        String memberId = "1";
-        Member member = Member.builder().id(memberId).userName("hi").age(25).build();
+    void memberBuildTest(){
+        Member member = Member.builder().name("songhae").age(25).role(Role.USER).description("I'm songhae").build();
         memberService.save(member);
-        Member findMember = memberService.findMember(memberId);
-        Assertions.assertThat(member.getUserName()).isEqualTo(findMember.getUserName());
+        Long findId = member.getId();
+        Member findMember = memberService.findMember(findId);
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void roleTimeTest(){
+        Member member = Member.builder().name("songhae").age(25).role(Role.USER).description("I'm songhae").build();
+        memberService.save(member);
+        Long findId = member.getId();
+        Member findMember = memberService.findMember(findId);
+        System.out.println("member.getDescription() = " + findMember.getDescription());
+        System.out.println("member.getRole() = " + findMember.getRole());
+        System.out.println("member.getCreatedDate() = " + findMember.getCreatedDate());
     }
 }
