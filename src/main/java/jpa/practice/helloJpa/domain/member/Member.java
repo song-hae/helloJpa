25daa2aabd817660df87c04a023a833e511c53a6
songhae <boricocke@naver.com>
2025-02-18 21:38:15 +0900

    연관관계 매핑 추가

--- src/main/java/jpa/practice/helloJpa/domain/member/Member.java
+++ src/main/java/jpa/practice/helloJpa/domain/member/Member.java
@@ -2,11 +2,13 @@ package jpa.practice.helloJpa.domain.member;
 
 import jakarta.persistence.*;
 import jpa.practice.helloJpa.domain.BaseTimeEntity;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jpa.practice.helloJpa.domain.order.Order;
+import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
+@Builder @AllArgsConstructor //Builder 패턴을 활용해 유연한 객체 생성 보장
 @Getter // Entity는 DB에 밀접해 있기 때문에 Setter를 통한 값 변환 막기 위해 만들지 않음
 @NoArgsConstructor // JPA가 기본 생성자를 통해 만들기 때문에 필요
 @Entity //JPA 매핑, 엔티티 클래스 선언
@@ -15,6 +17,7 @@ public class Member extends BaseTimeEntity {
 
     @Id //PK 설정
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
     // Entity가 영속 상태가 되려면 식별자가 반드시 필요한데, IDENTITY 전략은 Entity가 DB에 저장되어야 식별자를 구할 수 있다.
     // 따라서 영속 상태가 되는 즉시 insert가 SQL로 전달되기에 쓰기 지연이 동작하지 않는다.
     private Long id;
@@ -38,12 +41,18 @@ public class Member extends BaseTimeEntity {
     @Lob
     private String description;
 
-    @Builder //Builder 패턴을 활용해 유연한 객체 생성 보장
-    public Member(String name, Integer age, Role role, String description) {
-        this.name = name;
-        this.age = age;
-        this.role = role;
-        this.description = description;
-    }
+    @Column(nullable = false)
+    private String address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+//    @Builder //Builder 패턴을 활용해 유연한 객체 생성 보장
+//    public Member(String name, Integer age, Role role, String description) {
+//        this.name = name;
+//        this.age = age;
+//        this.role = role;
+//        this.description = description;
+//    }
 
 }
